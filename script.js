181a57b4f0174740c638cc684d43aa49d8752bcc
NaveenTej1997 <51020891+NaveenTej1997@users.noreply.github.com>
2020-03-08 14:25:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+/*var myImage=document.getElementById('mainImage');
+var imageArray=["1.jpg","2.jpg","3.jpg","4.jpg"];
+var imageIndex=1;
+function changeImage(){
+	myImage.setAttribute("src",imageArray[imageIndex]);
+	imageIndex++;
+	if(imageIndex>3)
+	{
+		imageIndex=0;
+	}
+}*/
+/*var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg'];
+
+    var x = 0;
+
+    var imgs = document.getElementById('img');
+
+    setInterval(slider, 2000);
+
+
+    function slider() {
+
+      if (x < images.length) {
+        x = x + 1;
+      } else {
+        x = 1;
+      }
+
+
+      imgs.innerHTML = "<img src=" + images[x - 1] + ">";
+
+
+    }*/
